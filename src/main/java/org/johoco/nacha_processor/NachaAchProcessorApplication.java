4a005b8105aafddef8e08f364c5d4ff96c72157e
John Hoestje <john.hoestje+GITHUB@gmail.com>
2024-09-03 14:02:10 -0500

    initial project shell

---
+++ src/main/java/org/johoco/nacha_processor/NachaAchProcessorApplication.java
@@ -0,0 +1,13 @@
+package org.johoco.nacha_processor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NachaAchProcessorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NachaAchProcessorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/johoco/nacha_processor/NachaAchProcessorApplicationTests.java
@@ -0,0 +1,13 @@
+package org.johoco.nacha_processor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NachaAchProcessorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
